fd9f7b22646272fe99e9d901d8de54964f5de8d5
hwnsng <amc214677@gmail.com>
2026-05-19 09:53:52 +0900

    feat: 파비콘 및 페이지 타이틀 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,16 @@ import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 import { ToastProvider } from "@/context/ToastContext";
 import { UserProvider } from "@/context/UserContext";
+import type { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "경소마고 귀가/귀교",
+  icons: {
+    icon: "/images/logo.png",
+  },
+};
 
 export default function RootLayout({
   children,
